d24fdd781ebec72db8bdfed285879afe1622dd6a
yuyahhh <yuyasakai6120@gmail.com>
2020-06-09 09:04:08 +0900

    userモデルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :birth_year_id, :birth_month_id, :birth_day_id, presence: true
+end
